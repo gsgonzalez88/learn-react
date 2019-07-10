7d8888905fd7ba2549249ce0c57b86491d60c493
gsgonzalez88 <gsgonzalez88@gmail.com>
2019-07-10 00:24:24 -0300

    working in a counter button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,8 +17,7 @@ function App() {
         >
           Learn React
         </a>
-        <Clock />
-        <Counter />
+        <Clock /><Counter />
       </header>
     </div>
   );
--- frontend/src/components/counter.jsx
+++ frontend/src/components/counter.jsx
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    
-    render() { 
+    state = {
+        count: 1
+    };
+
+    render() {
+
         return (
-            <h1>Hello World</h1>
+            /* JSX expression which eventually
+            gets compiled by Babel to calls to 
+            React.createElement. That's why we
+             have to import the React Obj  */
+            <div>
+                <span  className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-outline-light btn-sm">Increment</button>
+            </div>
         );
+    };
+
+    /* Rendering classes dinamically */
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "light";
+        return classes;
     }
-}
+
+    formatCount() {
+        let { count } = this.state;
+        return count === 0 ? 'Zero' : count;
+    };
+};
  
 export default Counter;
