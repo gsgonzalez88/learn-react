2aa807e959d560428fc01f49d71903ce4b3bbb50
gsgonzalez88 <gsgonzalez88@gmail.com>
2019-07-13 00:08:22 -0300

    exercice, adding a tablet with movies and button to delete with a fake DB

---
+++ frontend/src/components/movies.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { getMovies , deleteMovie } from '../services/fakeMovieService';
+
+class Movies extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            movies: getMovies()
+         };
+    }
+    handleDelete(movieID) {
+        
+        deleteMovie(movieID);
+        this.setState({ movies: getMovies()});
+    };
+    
+    renderMovie = () => {                
+       return this.state.movies.map(movie => {
+        return (<tr key={movie._id}>                
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td><button 
+                className="btn btn-danger btn-sm"
+                onClick={() => this.handleDelete(movie._id)}
+                >
+                Delete
+                </button>
+                </td>
+        </tr>);
+        });
+    };
+  
+    msgShowedMovies = () => {
+        let numberOfMovies = this.state.movies.length;
+        return numberOfMovies === 0 ? 'No movies to show' :  `Showing ${numberOfMovies} movies`;
+    }
+
+    render() { 
+        return ( 
+            <div>
+            <p>{this.msgShowedMovies()}</p>            
+            <table className="table">
+            <thead>
+              <tr>                
+                <th scope="col">Title</th>
+                <th scope="col">Genre</th>
+                <th scope="col">Stock</th>
+                <th scope="col">Rate</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+             {this.renderMovie()}
+            </tbody>
+            </table>
+            </div>
+         );
+    }
+}
+ 
+export default Movies;
